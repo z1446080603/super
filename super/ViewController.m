37ac1ce6f5ac9d04da88d3559864b58b3fd8522d
z1446080603 <1446080603@qq.com>
2017-09-12 16:26:25 +0800

    first commit

--- super/ViewController.m
+++ super/ViewController.m
@@ -16,14 +16,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+   
+    NSLog(@"123");
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 
 @end
